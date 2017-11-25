b4cce6ce6bb196202992758ca42af19963e76699
onkyoworm <996570982@qq.com>
2017-11-25 23:06:43 +0800

    update

---
+++ AutoCatcher.py
@@ -0,0 +1,35 @@
+#coding: utf-8
+
+import os
+import argparse
+import threading
+import Queue
+import re
+import subprocess
+
+
+#defind global arguments
+sqlmap_url_list = list()
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--sqlmap", dest="sqlmap", help="input the file name contain URLs(Also arguments)", type=str)
+
+	args = parser.parse_args()
+
+#default sqlmap's command:
+#python sqlmap.py -u "target url" --threads=10 --tamper=randomcomments --random-agent --level=5 
+	try:
+		with open(args.sqlmap, 'r') as f:
+			for url in f.readlines():
+				#check the URL format
+				if url[0:7] == 'http://':
+					url = re.sub('\s', '',url)
+					sqlmap_url_list.append(url)
+				#read urls, and output for commandlins's
+				for i in sqlmap_url_list: 
+					output_commands = 'python sqlmap.py -u ' + i + '--threads=10 --tamper=randomcomments --random-agent --level=5'
+					process = subprocess.Popen(output_commands, stdin=subprocess.PIPE, 	stdout= subprocess.PIPE, stderr= subprocess.PIPE, shell= False)
+					print process.stdout.read()
+	except Exception as e:
+		print e
